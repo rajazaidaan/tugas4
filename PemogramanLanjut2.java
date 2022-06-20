ae1dcbf87618e9efa8d8416c70d66c967246ed1a
rajazaidaan <56380732+rajazaidaan@users.noreply.github.com>
2022-06-20 15:44:41 +0700

    Add files via upload

---
+++ PemogramanLanjut2.java
@@ -0,0 +1,16 @@
+
+public class PemogramanLanjut2 {
+    String nopolisi;
+    String model;
+    int TahunProduksi;
+    String Warna;
+    
+    public PemogramanLanjut2(String mdl, String nopol, int tahun){
+       nopolisi = nopol;
+       mdl = model;
+       TahunProduksi = tahun;  
+    }
+    public static void main(String[] args){
+        System.out.println("Hello world");
+    }
+}
